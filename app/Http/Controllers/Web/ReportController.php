7d7bfb624b95cc187afe7dd4747d9a9569970ad3
Julius <julius.inalkom@gmail.com>
2025-10-02 09:18:59 +0700

    unauthorized staff

--- app/Http/Controllers/Web/ReportController.php
+++ app/Http/Controllers/Web/ReportController.php
@@ -14,16 +14,20 @@ use Illuminate\Http\Request;
 class ReportController extends Controller
 {
     use AuthorizesRequests;
+
+    public function __construct()
+    {
+        $this->authorize('view-reports');
+    }
+
     public function revenue()
     {
-        $this->authorize('viewAny', \App\Models\Invoice::class); // Reuse Invoice policy for report access
         $areas = Area::all();
         return view('pages.reports.revenue', compact('areas'));
     }
 
     public function staffPerformance()
     {
-        $this->authorize('viewAny', \App\Models\Staff::class);
         $areas = Area::all();
         $staff = \App\Models\Staff::with('user')->get();
         return view('pages.reports.staff-performance', compact('areas', 'staff'));
@@ -31,48 +35,40 @@ class ReportController extends Controller
 
     public function customerGrowth()
     {
-        $this->authorize('viewAny', \App\Models\Customer::class);
         $areas = Area::all();
         return view('pages.reports.customer-growth', compact('areas'));
     }
 
     public function revenueDrilldown(\App\Models\ServiceCategory $serviceCategory)
     {
-        $this->authorize('viewAny', \App\Models\Invoice::class);
         return view('pages.reports.revenue-drilldown', compact('serviceCategory'));
     }
 
     public function staffDrilldown(\App\Models\Staff $staff)
     {
-        $this->authorize('view', $staff);
         return view('pages.reports.staff-drilldown', compact('staff'));
     }
 
     public function customerDrilldown($customerId)
     {
         $customer = \App\Models\Customer::withTrashed()->findOrFail($customerId);
-        $this->authorize('view', $customer);
         return view('pages.reports.customer-drilldown', compact('customer'));
     }
 
     public function profitability(Request $request)
     {
-        $this->authorize('viewAny', Invoice::class); // Reuse policy
         $areas = Area::all();
         return view('pages.reports.profitability', compact('areas'));
     }
 
     public function staffUtilization(Request $request)
     {
-        $this->authorize('viewAny', Staff::class); // Reuse policy
         $areas = Area::all();
         return view('pages.reports.staff_utilization', compact('areas'));
     }
 
     public function invoiceAging(Request $request)
     {
-        $this->authorize('viewAny', Invoice::class);
-
         $invoices = Invoice::with('serviceOrder.customer')
             ->whereIn('status', [Invoice::STATUS_SENT, Invoice::STATUS_OVERDUE])
             ->get()
