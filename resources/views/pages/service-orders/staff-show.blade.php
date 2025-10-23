0d0812141fd666287004de83ff99b8e33f7e5695
Panjul <julius.inalkom@gmail.com>
2025-10-23 14:17:07 +0700

    muat ulang button on staff page

--- resources/views/pages/service-orders/staff-show.blade.php
+++ resources/views/pages/service-orders/staff-show.blade.php
@@ -19,6 +19,9 @@
                 <h2 class="page-title">Detail Service Order: {{ $serviceOrder->so_number }}</h2>
             </div>
             <div class="col-auto ms-auto d-print-none">
+                <button type="button" class="btn btn-warning text-dark me-2" onclick="window.location.reload();">
+                    Muat Ulang
+                </button>
                 @if($serviceOrder->status == 'booked')
                     <button class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#startWorkModal">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /><path d="M12 7l-2 4l2 4l2 -4l-2 -4" /></svg>
